c3859cce22ad7c98210377c9f1fa51f34fbebfa0
kollydap <dapkolly@gmail.com>
2025-02-21 09:54:50 +0100

    adjusted models

--- gemwallets/models.py
+++ gemwallets/models.py
@@ -4,23 +4,29 @@ import uuid
 
 User = get_user_model()
 
-class GemWallet(models.Model):
-    STATE_CHOICES = [
-        ('ACTIVE', 'Active'),
-        ('SUSPENDED', 'Suspended'),
-        ('CLOSED', 'Closed'),
-    ]
-    STATUS_CHOICES = [
-        ('PENDING', 'Pending'),
-        ('APPROVED', 'Approved'),
-        ('DECLINED', 'Declined'),
-    ]
 
+class GemState(models.TextChoices):
+    ACTIVE = "active", "Active"
+    SUSPENDED = "suspended", "Suspended"
+    CLOSED = "closed", "Closed"
+
+
+class GemStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    APPROVED = "approved", "Approved"
+    DECLINED = "declined", "Declined"
+
+
+class GemWallet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='wallet')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wallet")
     balance = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
-    state = models.CharField(max_length=20, choices=STATE_CHOICES, default='ACTIVE')
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='APPROVED')
+    state = models.CharField(
+        max_length=20, choices=GemState.choices, default=GemState.ACTIVE
+    )
+    status = models.CharField(
+        max_length=20, choices=GemStatus.choices, default=GemStatus.APPROVED
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
