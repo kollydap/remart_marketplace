d6720b98f2e61f869afe35583384fc8576f5f7c8
kollydap <dapkolly@gmail.com>
2025-04-22 00:44:17 +0100

    Add Swagger UI endpoint for API documentation

--- remart/urls.py
+++ remart/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import CustomRegisterView, CustomLoginView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version="v1",
+        description="Your API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -18,4 +36,9 @@ urlpatterns = [
     path(
         "api/v1/auth/registration/", include("dj_rest_auth.registration.urls")
     ),  # Default registration
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
