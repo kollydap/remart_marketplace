a821cc3d0fdcdcc2e764bccaa1fdb5c3f503be93
kollydap <dapkolly@gmail.com>
2025-04-23 13:43:39 +0100

    Implement get_order_quantity method in TransactionSerializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -6,6 +6,7 @@ class TransactionSerializer(serializers.ModelSerializer):
     sender_name = serializers.SerializerMethodField()
     receiver_name = serializers.SerializerMethodField()
     product_name = serializers.SerializerMethodField()
+    order_quantity = serializers.SerializerMethodField()
 
     class Meta:
         model = Transaction
@@ -23,6 +24,7 @@ class TransactionSerializer(serializers.ModelSerializer):
             "sender_name",
             "receiver_name",
             "product_name",
+            "order_quantity",
         ]
 
     def get_sender_name(self, obj):
@@ -36,3 +38,9 @@ class TransactionSerializer(serializers.ModelSerializer):
             return obj.order.product.name
         except AttributeError:
             return "Unknown Product"
+
+    def get_order_quantity(self, obj):
+        try:
+            return obj.order.quantity
+        except AttributeError:
+            return "Unknown Quantity"
